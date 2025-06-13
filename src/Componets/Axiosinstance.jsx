b6b90b5da1effd124232b49e20f4fca49e77365c
EMGS <getahuntamirat08@gmail.com>
2025-06-13 11:06:25 -0700

    delete some file

--- src/Componets/Axiosinstance.jsx
+++ src/Componets/Axiosinstance.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const baseUrl = 'http://127.0.0.1:8000/';
+
 const isDevelopement=import.meta.env.MODE ==='development'
 const baseUrl=isDevelopement? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
 const AxiosInstance = axios.create({
